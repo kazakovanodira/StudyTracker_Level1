b388402b695c2f179c8cda6df36e24932378a26a
kazakovanodira <kazakovanodira04@gmail.com>
2025-10-16 10:59:59 -0400

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace StudyTracker_Level1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
