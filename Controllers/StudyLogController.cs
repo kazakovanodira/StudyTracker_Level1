72c900064fc3e0a6413d9a2eef5da9df1b766260
kazakovanodira <kazakovanodira04@gmail.com>
2026-03-14 14:20:08 -0400

    Add StudyLogController.cs

---
+++ Controllers/StudyLogController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudyTracker_Level1.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class StudyLogController : ControllerBase
+{
+    public StudyLogController()
+    {
+        
+    }
+    
+    
+}
